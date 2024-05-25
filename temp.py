84af29bbe062e9af933a340e767d8e05d5668143
eknirmata <eknirmata@outlook.com>
2024-05-25 18:46:55 -0400

    Update temp.py
    .

--- temp.py
+++ temp.py
@@ -1,6 +1,30 @@
+# Function to get pod information from a project
+def get_pods_info(dyn_client, project_name):
+    v1_pods = dyn_client.resources.get(api_version='v1', kind='Pod')
+    pod_list = v1_pods.get(namespace=project_name)
+    pods_info = []
+    for pod in pod_list.items:
+        containers_info = []
+        for container in pod.spec.containers:
+            containers_info.append({
+                'container_name': container.name,
+                'container_image': container.image
+            })
+        pods_info.append({
+            'pod_name': pod.metadata.name,
+            'node_name': pod.spec.nodeName,  # Node name where the pod is running
+            'containers': containers_info,
+            'create_time': pod.metadata.creationTimestamp
+        })
+    return pods_info
+
+
+
+#--
+
 # Prepare the CSV file for writing the output
 with open('cluster_projects_pods.csv', mode='w', newline='') as file:
-    fieldnames = ['cluster', 'project_name', 'pod_name', 'container_name', 'container_image', 'create_time']
+    fieldnames = ['cluster', 'project_name', 'pod_name', 'node_name', 'container_name', 'container_image', 'create_time']
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     writer.writeheader()  # Write the header
 
@@ -22,30 +46,8 @@ with open('cluster_projects_pods.csv', mode='w', newline='') as file:
                             'cluster': row['apihost'],
                             'project_name': project_name,
                             'pod_name': pod_info['pod_name'],
+                            'node_name': pod_info['node_name'],  # Include node name in the output
                             'container_name': container_info['container_name'],
                             'container_image': container_info['container_image'],
                             'create_time': pod_info['create_time']
                         })
-
-# ===
-
-# Function to get pod information from a project
-def get_pods_info(dyn_client, project_name):
-    v1_pods = dyn_client.resources.get(api_version='v1', kind='Pod')
-    pod_list = v1_pods.get(namespace=project_name)
-    pods_info = []
-    for pod in pod_list.items:
-        containers_info = []
-        for container in pod.spec.containers:
-            containers_info.append({
-                'container_name': container.name,
-                'container_image': container.image
-            })
-        pods_info.append({
-            'pod_name': pod.metadata.name,
-            'node_name': pod.spec.nodeName,  # Node name where the pod is running
-            'containers': containers_info,
-            'create_time': pod.metadata.creationTimestamp
-        })
-    return pods_info
-
